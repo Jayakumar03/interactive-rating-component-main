e3f98a3f66109594ee6f5536188db9c2b04c40b7
jayakumar03 <jaimech61@gmail.com>
2023-02-10 18:11:15 +0530

    added display for card

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-console.log("this works")
-// holding variables
+
 const numberBtn = document.getElementsByClassName("indi-number")
+const submitBtn = document.getElementById("submit-btn")
+const ratingDisplay = document.getElementsByClassName("main-container")
 
 
 
+for (let index = 0; index <= numberBtn.length; index++) {
+    numberBtn[index] = addEventListener("click",  changeBackgroundColor )
+    
+}
 
+submitBtn.addEventListener("click", changeDisplay)
 
-// event listener
-
-numberBtn.addEventListener("click ", backGroundColorChanger())
-
-
-
-
-// function
-
-function backGroundColorChanger () {
-    numberBtn.style.backgroundColor = "hsl(25, 97%, 53%)"
+function changeBackgroundColor() {
+    numberBtn.style.backgroundColor = "red"
     numberBtn.style.color = "white"
+  
+}
+
+function changeDisplay() {
+    ratingDisplay[0].style.display = "none"
+    ratingDisplay[1].style.display = "block"
 
 }
+
+// todo
+// 1) display the ratingDisplay
+// 2) even after refreshing the dom should the change
