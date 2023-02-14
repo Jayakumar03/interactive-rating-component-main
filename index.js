7e188f5a8775e59a2ba35928169419aa0964a186
jayakumar03 <jaimech61@gmail.com>
2023-02-14 22:20:48 +0530

    Updated function  changeBackgroundColor

--- index.js
+++ index.js
@@ -2,27 +2,41 @@
 const numberBtn = document.getElementsByClassName("indi-number")
 const submitBtn = document.getElementById("submit-btn")
 const ratingDisplay = document.getElementsByClassName("main-container")
+const rating = document.getElementById("selected-rating")
 
 
 
-for (let index = 0; index <= numberBtn.length; index++) {
+// Event listener
+
+for (let index = 0; index < numberBtn.length; index++) {
     numberBtn[index] = addEventListener("click",  changeBackgroundColor )
-    
+    console.log(index)
 }
 
 submitBtn.addEventListener("click", changeDisplay)
 
+
+
+
+// Function
+
 function changeBackgroundColor() {
-    numberBtn.style.backgroundColor = "red"
-    numberBtn.style.color = "white"
+    let val =  numberBtn.innerText
+    numberBtn[val].style.backgroundColor = "red"
+    numberBtn[val].style.color = "white"
+    // changeDisplay(4)
   
 }
 
-function changeDisplay() {
+function changeDisplay(val) {
     ratingDisplay[0].style.display = "none"
     ratingDisplay[1].style.display = "block"
+    rating.innerText = val
+
 
 }
+
+
 
 // todo
 // 1) display the ratingDisplay
